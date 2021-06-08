46d70b5e8134e5de3536056e37d074a55ef8e458
liukefu2050 <liukefu2050@sina.com>
2021-06-08 12:40:39 +0800

    修改包名

--- xs-common/src/main/java/com/xiushang/jpa/repository/BaseDao.java
+++ xs-common/src/main/java/com/xiushang/jpa/repository/BaseDao.java
@@ -2,11 +2,13 @@ package com.xiushang.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * 数据访问层基类
  * Created by kf on 2016/11/7.
  */
+@NoRepositoryBean
 public interface BaseDao<T> extends
         JpaRepository<T, String>,
         QuerydslPredicateExecutor<T> {
