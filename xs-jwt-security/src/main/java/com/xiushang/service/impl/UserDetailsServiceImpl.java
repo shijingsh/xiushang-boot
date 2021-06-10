f94cd36e55368cb6054dea2db97325d33bd99b8c
liukefu2050 <liukefu2050@sina.com>
2021-06-10 15:29:56 +0800

    jwt

---
+++ xs-jwt-security/src/main/java/com/xiushang/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.xiushang.service.impl;
+
+import com.xiushang.entity.UserEntity;
+import com.xiushang.jpa.repository.UserDao;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
+/**
+ * @author zhaoxinguo on 2017/9/13.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserDao userDao;
+
+    /**
+     * 通过构造器注入userDao
+     * @param userDao
+     */
+    public UserDetailsServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userDao.findByName(username);
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getLoginName(), user.getPassword(), emptyList());
+    }
+
+}
