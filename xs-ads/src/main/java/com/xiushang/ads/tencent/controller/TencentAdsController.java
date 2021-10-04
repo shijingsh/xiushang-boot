59a67ec76539c5a47bc4b366893b7096d2e2a3e3
liukefu2050 <liukefu2050@sina.com>
2021-10-04 10:40:43 +0800

    ads

---
+++ xs-ads/src/main/java/com/xiushang/ads/tencent/controller/TencentAdsController.java
@@ -0,0 +1,16 @@
+package com.xiushang.ads.tencent.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Api(tags = "广点通接口")
+@Controller
+@RequestMapping(value = "/tencent",
+        produces = "application/json; charset=UTF-8")
+public class TencentAdsController {
+
+
+
+
+}
