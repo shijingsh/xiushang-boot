fd518d1538c503bde64f3aea476fe738e7a995af
liukefu2050 <liukefu2050@sina.com>
2021-11-24 16:47:10 +0800

    auth2 自定义登陆

--- xs-jwt-security/src/main/java/com/xiushang/security/authentication/mobile/SmsCodeFilter.java
+++ xs-jwt-security/src/main/java/com/xiushang/security/authentication/mobile/SmsCodeFilter.java
@@ -15,7 +15,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
