81e59dc366ff1fe9ca70436de43085cdaf7dbfa7
liukefu2050 <liukefu2050@sina.com>
2022-01-11 17:02:44 +0800

    xs-validation

--- xiushang-boot-example/example-admin/src/main/java/com/xiushang/admin/validation/controller/ValidateController.java
+++ xiushang-boot-example/example-admin/src/main/java/com/xiushang/admin/validation/controller/ValidateController.java
@@ -4,24 +4,34 @@ import com.xiushang.admin.validation.vo.ParamVo;
 import com.xiushang.framework.log.CommonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 
 @Api(tags = {"常用接口"})
+@Validated
 @Controller
 @RequestMapping(value = "/api/valid",
         produces = "application/json; charset=UTF-8")
 public class ValidateController {
 
-	@ApiOperation(value = "测试参数校验")
-    @PostMapping(value = "/testNull")
+	@ApiOperation(value = "ApiModelProperty必填校验")
+    @PostMapping(value = "/apiModelProperty")
 	@ResponseBody
-    public CommonResult val(@Valid @RequestBody ParamVo paramVo) {
+    public CommonResult valApiModelProperty(@Valid @RequestBody ParamVo paramVo) {
 
         return CommonResult.success();
     }
 
+    @ApiOperation(value = "ApiParam必填校验")
+    @GetMapping(value = "/apiParam")
+    @ResponseBody
+    public CommonResult valApiParam(@ApiParam(value = "id",required = true) String id) {
+
+        return CommonResult.success();
+    }
 }
