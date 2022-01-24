e62d06c4cb315cc62c0f16c510c240de5636f4a6
liukefu2050 <liukefu2050@sina.com>
2022-01-24 10:38:36 +0800

    getFull

--- xs-service/src/main/java/com/xiushang/common/utils/BaseServiceImpl.java
+++ xs-service/src/main/java/com/xiushang/common/utils/BaseServiceImpl.java
@@ -8,6 +8,7 @@ import com.xiushang.jpa.repository.BaseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -39,6 +40,7 @@ public abstract class BaseServiceImpl<T> {
         return null;
     }
 
+    @Transactional(readOnly = true)
     public T getFull(String id){
         Optional<T> optional = baseDao.findById(id);
         if(optional.isPresent()){
