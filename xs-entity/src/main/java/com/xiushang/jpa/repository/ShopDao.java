6388f5550d1353681cf72bb85552b78c4cc6e8ca
liukefu <liukefu2020@sina.com>
2022-04-20 14:40:30 +0800

    导入修改

--- xs-entity/src/main/java/com/xiushang/jpa/repository/ShopDao.java
+++ xs-entity/src/main/java/com/xiushang/jpa/repository/ShopDao.java
@@ -3,7 +3,13 @@ package com.xiushang.jpa.repository;
 import com.xiushang.entity.UserEntity;
 import com.xiushang.entity.shop.ShopEntity;
 
+import java.util.List;
+
 public interface ShopDao extends BaseDao<ShopEntity> {
 
     ShopEntity findByOwnerUser(UserEntity ownerUser);
+
+    ShopEntity findByCode(String code);
+
+    List<ShopEntity> findByCompanyName(String companyName);
 }
