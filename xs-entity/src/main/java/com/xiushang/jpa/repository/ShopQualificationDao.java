f513e589c8d83a52a41b27152d2d2c779163788f
liukefu2050 <liukefu2050@sina.com>
2023-02-08 15:36:40 +0800

    实名认证关联用户

--- xs-entity/src/main/java/com/xiushang/jpa/repository/ShopQualificationDao.java
+++ xs-entity/src/main/java/com/xiushang/jpa/repository/ShopQualificationDao.java
@@ -1,8 +1,9 @@
 package com.xiushang.jpa.repository;
 
-import com.xiushang.entity.shop.ShopEntity;
+import com.xiushang.entity.UserEntity;
 import com.xiushang.entity.shop.ShopQualificationsEntity;
 
 public interface ShopQualificationDao extends BaseDao<ShopQualificationsEntity> {
-    ShopQualificationsEntity findByBelongShop(ShopEntity shopEntity);
+
+    ShopQualificationsEntity findByUser(UserEntity user);
 }
